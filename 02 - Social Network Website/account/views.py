27b919d2c7130d3fbd9981478ffd63a1c1fbfbb7
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-08-11 12:15:07 +0330

    crated form for registration and also these files : register.html, view.UserRegisterView and added functionallity with the related url patterns

--- 02 - Social Network Website/account/views.py
+++ 02 - Social Network Website/account/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from .forms import *
 
 # Create your views here.
 class RegisterView(View):
     def get(self, request):
-        pass
+        form = UserRegisterForm()
+        return render(request, "account/register.html", {"form": form})
 
     def post(self, request):
         pass
