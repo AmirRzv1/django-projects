432b22aaadd90a3434ca516c1de6d16c86ce2444
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-08-11 13:10:45 +0330

    created a costume form for my account app and add some bootstrap class to it for better vewing.

--- 02 - Social Network Website/account/forms.py
+++ 02 - Social Network Website/account/forms.py
@@ -1,6 +1,9 @@
 from django  import forms
 
 class UserRegisterForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField()
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control","placeholder": "SidAmir"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control","placeholder": "s.amirhosein.rzv@gmail.com"}))
+    # help_text --> this will be shown near the input when its being rendered
+    # widget --> PasswordInput -> this is an extra feature for our form that hide
+    # the password we are entering in the form that being shown to us
+    password = forms.CharField(help_text="Dont Use : #$%^&*", widget=forms.PasswordInput(attrs={"class": "form-control"}))
