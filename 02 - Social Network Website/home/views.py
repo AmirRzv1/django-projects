c5c24619d599a011930dd3196080ae7285b02789
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-08-11 11:54:02 +0330

    added first class base view and connect it to url properly

--- 02 - Social Network Website/home/views.py
+++ 02 - Social Network Website/home/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
-def home(request):
-    # This kind of mapping for temp files is because we made an inner
-    # folder named templates inside our app and it has a html file
-    # and we want to access to it so first the name of the folder in templates
-    # then the exact name of our template
-    # the home i mentioned here is not my app name its the name of my
-    # folder inside the templates folder in home app
-    return render(request, "home/index.html")
+# Camel Case naming for classes
+class HomeView(View):
+    def get(self, request):
+        # This kind of mapping for temp files is because we made an inner
+        # folder named templates inside our app and it has a html file
+        # and we want to access to it so first the name of the folder in templates
+        # then the exact name of our template
+        # the home i mentioned here is not my app name its the name of my
+        # folder inside the templates folder in home app
+        return render(request, "home/index.html")
