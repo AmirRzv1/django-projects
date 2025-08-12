0424d516274f56364049bfd4d91f8789769eeaff
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-08-12 14:18:42 +0330

    created complete view for registration user and save it to db

--- 02 - Social Network Website/account/apps.py
+++ 02 - Social Network Website/account/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# Using this name in setting file for adding this to my project
 class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'account'
--- 02 - Social Network Website/account/views.py
+++ 02 - Social Network Website/account/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import *
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 class RegisterView(View):
+    # with get we show the form to user, in order to fill the information
     def get(self, request):
         form = UserRegisterForm()
         return render(request, "account/register.html", {"form": form})
 
+    # with post we collect the data from our user and start validating
     def post(self, request):
-        pass
+        # we need to pass the request.POST to our form to use its functionality
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            # data which is valid and we receive it from our form
+            cd = form.cleaned_data
+            username = cd["username"]
+            email = cd["email"]
+            password = cd["password"]
+            User.objects.create_user(username=username, email=email, password=password)
+            messages.success(request, "You Registered Successfully !", "success")
+            # first namespace then the url name
+            return redirect("home:home")
