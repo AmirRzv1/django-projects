d79d566e6edcc2df87d89189bf04a41543dd6b1e
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-08-12 14:42:23 +0330

    complete the register functionallity for this project so users can register just fine and the error handeling is good too.

--- 02 - Social Network Website/account/views.py
+++ 02 - Social Network Website/account/views.py
@@ -6,15 +6,20 @@ from django.contrib import messages
 
 # Create your views here.
 class RegisterView(View):
+    # we use this kind of naming as a class variable so we stop repeating our self
+    form_class = UserRegisterForm
+    # we use template name a lot so we set this as a class variable
+    template_name = "account/register.html"
+
     # with get we show the form to user, in order to fill the information
     def get(self, request):
-        form = UserRegisterForm()
-        return render(request, "account/register.html", {"form": form})
+        form = self.form_class()
+        return render(request, self.template_name, {"form": form})
 
     # with post we collect the data from our user and start validating
     def post(self, request):
         # we need to pass the request.POST to our form to use its functionality
-        form = UserRegisterForm(request.POST)
+        form = self.form_class(request.POST)
         if form.is_valid():
             # data which is valid and we receive it from our form
             cd = form.cleaned_data
@@ -25,3 +30,5 @@ class RegisterView(View):
             messages.success(request, "You Registered Successfully !", "success")
             # first namespace then the url name
             return redirect("home:home")
+        # if the input vars from user is invalid so we show the page again with the right error message
+        return render(request, self.template_name, {"form": form})
