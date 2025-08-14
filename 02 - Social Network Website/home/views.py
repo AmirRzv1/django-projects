63a1591c6cfe1fba8152b9b6337e44e0452c49a1
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-08-14 12:02:34 +0330

    add a part in my home page to show all the posts and some edit on the templates in showing

--- 02 - Social Network Website/home/views.py
+++ 02 - Social Network Website/home/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.views import View
+from post.models import Post
 
 # Camel Case naming for classes
 class HomeView(View):
     def get(self, request):
+        posts = Post.objects.all()
         # This kind of mapping for temp files is because we made an inner
         # folder named templates inside our app and it has a html file
         # and we want to access to it so first the name of the folder in templates
         # then the exact name of our template
         # the home i mentioned here is not my app name its the name of my
         # folder inside the templates folder in home app
-        return render(request, "home/index.html")
+        return render(request, "home/index.html", {"posts": posts})
+
