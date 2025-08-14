aca8283bc94951305769a0ce6c5e97531ed050d1
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-08-14 10:41:26 +0330

    create a model in post app and register it in admin paneel as well.

--- 02 - Social Network Website/post/models.py
+++ 02 - Social Network Website/post/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    # make relation to the User model so the post connect to one user
+    # each user many post - each post belong to one user
+    # OneToMany Relation
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    # this is a string for reading urls better
+    slug = models.SlugField()
+    # first created - only once
+    created = models.DateTimeField()
+    # each updates for post - multiple time
+    updated = models.DateTimeField()
