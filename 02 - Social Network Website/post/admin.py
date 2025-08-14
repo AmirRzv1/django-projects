caa390e2e2279bef69ecdb92b0a54dc99ffada83
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-08-14 11:46:25 +0330

    create a costume admin panel for Post model

--- 02 - Social Network Website/post/admin.py
+++ 02 - Social Network Website/post/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.
-admin.site.register(Post)
+# creating the costume admin here
+# way 1 -> we also can register the setting with the decorator
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # it tells django which fields we want to show in admin panel
+    list_display = ["user", "slug", "updated", "created"]
+    # it show a bar in admin panel and let us to search in slug
+    search_fields = ["slug"]
+    # to filter each field we want, here base on date for updated
+    list_filter = ["updated"]
+    # how to fill slug based on other fields
+    prepopulated_fields = {"slug": ["body"]}
+    # let us to see the whole users and select the one we want
+    raw_id_fields = ["user"]
+
+# way 2 --> we can register the model and the setting for our admin panel like this
+# after defining the class and its feature add the class name here
+# admin.site.register(Post, PostAdmin)
