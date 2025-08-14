a7bf06864fbcc32aa74270bc89c3b5251010a00e
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-08-14 12:13:23 +0330

    create a part for showing detail for each post

--- 02 - Social Network Website/post/urls.py
+++ 02 - Social Network Website/post/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import *
 
 app_name = "post"
 urlpatterns = [
+    path("post/<int:post_id>/<slug:post_slug>/", PostDetailView.as_view(), name="post_detail"),
 
 ]
