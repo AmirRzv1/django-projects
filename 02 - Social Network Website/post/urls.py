5fb0bd62557a2805b68046c79190919919938b18
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-08-16 17:09:42 +0330

    add some fucntionallity so the users can delete their post and see the button delete beside each pst!

--- 02 - Social Network Website/post/urls.py
+++ 02 - Social Network Website/post/urls.py
@@ -4,5 +4,6 @@ from .views import *
 app_name = "post"
 urlpatterns = [
     path("post/<int:post_id>/<slug:post_slug>/", PostDetailView.as_view(), name="post_detail"),
+    path("post_delete/<int:post_id>/", PostDeleteView.as_view(), name="post_delete"),
 
 ]
--- 02 - Social Network Website/post/views.py
+++ 02 - Social Network Website/post/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 # Create your views here.
 class PostDetailView(View):
@@ -10,3 +12,15 @@ class PostDetailView(View):
 
     def post(self, request):
         pass
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def post(self, request, post_id):
+        user = request.user.id
+        owner = Post.objects.get(pk=post_id)
+        if user == owner.user.id:
+            Post.objects.remove(id=post_id)
+            messages.success(request, "Post Deleted Successfully !", "success")
+            return redirect("home:home")
+        messages.warning(request, "You are not the owner of the post !")
+        return redirect("home:home")
+
