34709ffdfecccd93953118db99d7444162115eb3
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-08-20 11:33:36 +0330

    added a new model Comment for commenting

--- 02 - Social Network Website/post/models.py
+++ 02 - Social Network Website/post/models.py
@@ -35,3 +35,21 @@ class Post(models.Model):
     # in out templates
     def get_absolute_url(self):
         return reverse("post:post_detail", args=[self.id, self.slug])
+
+class Comment(models.Model):
+    # which user is commenting
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ucomments")
+    # which post is getting a comment
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="pcomments")
+    # inner comment for reply - we need to connect the Comment model to itself
+    # tip : not all the comments are reply, some of them are the main comment so
+    # we need to create it empty first
+    reply = models.ForeignKey("self", models.CASCADE, related_name="rcomments", blank=True, null=True)
+    # the comment is reply or not
+    is_reply = models.BooleanField(default=False)
+    # the comment itself
+    body = models.TextField(max_length=400)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.body[:30]}"
