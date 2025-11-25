70d841575f974e82995a1d20bfc011ce828a1d18
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-11-25 11:23:19 +0330

    created my models for moview and review

--- 03 - Movie Rating APP (DRF)/review/models.py
+++ 03 - Movie Rating APP (DRF)/review/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=30)
+    director = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
+    # this validators always work and they force the user to enter this range of numbers
+    rate = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+    comment = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
