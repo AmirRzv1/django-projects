1e5d92af8c98832748e97c27ee40c2bce92d1541
Amir Razavi <s.amirhosein.rzv@gmail.com>
2026-01-27 16:58:49 +0330

    created the accounts app and essentials parts.

---
+++ 04 - Task Management System/accounts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+class UserRegisterView(View):
+    def get(self, request):
+        return render(request, "accounts/register.html")
+
+    def post(self, request):
+        pass
