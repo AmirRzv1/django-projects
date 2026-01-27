973ec48f47019fd6568ee80bf03a56b2d9fa2672
Amir Razavi <s.amirhosein.rzv@gmail.com>
2026-01-27 21:45:51 +0330

    almost everything needed for POST and register a user is done now.

--- 04 - Task Management System/accounts/views.py
+++ 04 - Task Management System/accounts/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import *
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 class UserRegisterView(View):
+    form_class = UserRegisterForm
+
     def get(self, request):
-        form = UserRegisterForm()
+        form = self.form_class()
         return render(request, "accounts/register.html", {"form": form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            try:
+                User.objects.create_user(username=data["username"], password=data["password"])
+            except IntegrityError:
+                form.add_error('username', 'Username already exists')
+                return render(request, 'accounts/register.html', {'form': form})
+
+            return render(request, "core/landing.html")
+
+
