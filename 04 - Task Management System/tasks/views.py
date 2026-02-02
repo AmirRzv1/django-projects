5301ad20a44bee4b79e50f7e67feb2abb3dbacca
Amir Razavi <s.amirhosein.rzv@gmail.com>
2026-02-02 12:28:01 +0330

    set authentication deeply for the users which are not logged in so they can not access the dashboard with the direct url.

--- 04 - Task Management System/tasks/views.py
+++ 04 - Task Management System/tasks/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class TaskDashboardView(View):
+class TaskDashboardView(LoginRequiredMixin, View):
+    login_url = "accounts:user_login"  # redirect here if not logged in
+
     def get(self, request):
         return render(request, "tasks/dashboard.html")
