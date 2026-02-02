184dd4ff2ea61af9d77fbf621094a066b2d106b2
Amir Razavi <s.amirhosein.rzv@gmail.com>
2026-02-02 14:13:36 +0330

    add some changes to the templates and complete the create task page and action of it.

--- 04 - Task Management System/tasks/views.py
+++ 04 - Task Management System/tasks/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import *
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
 class TaskDashboardView(LoginRequiredMixin, View):
@@ -10,5 +13,26 @@ class TaskDashboardView(LoginRequiredMixin, View):
         return render(request, "tasks/dashboard.html")
 
 class TaskCreateView(View):
+    form_class = TasksCreateForm
+
     def get(self, request):
-        return render(request, "tasks/task_create.html")
+        form = self.form_class()
+        return render(request, "tasks/task_create.html", {"form": form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Task.objects.create(title=data["title"], description=data["description"], owner=request.user)
+            messages.success(request, "Task created Successfully.")
+            return redirect("tasks:task_dashboard")
+        return render(request, "tasks/dashboard.html", {"form": form})
+
+
+
+
+
+
+
+
+
