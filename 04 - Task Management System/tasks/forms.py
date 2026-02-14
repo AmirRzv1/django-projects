b1a14acc8d52c734ce175d4c2eb09f1e75a52513
Amir Razavi <s.amirhosein.rzv@gmail.com>
2026-02-14 08:20:07 +0330

    now the login is fine and also update dont show the soft delete any more.

--- 04 - Task Management System/tasks/forms.py
+++ 04 - Task Management System/tasks/forms.py
@@ -9,3 +9,11 @@ class TaskUpdateForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["title", "description", "status"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["status"].choices = [
+            ("ongoing", "OnGoing"),
+            ("completed", "Completed")
+        ]
