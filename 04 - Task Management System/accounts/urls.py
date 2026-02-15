8f13238c0a1aeb9e660381af3f2f52f647c71022
Amir Razavi <s.amirhosein.rzv@gmail.com>
2026-02-15 18:22:12 +0330

    create and configure the password reset

--- 04 - Task Management System/accounts/urls.py
+++ 04 - Task Management System/accounts/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="user_register"),
     path("login/", UserLoginView.as_view(), name="user_login"),
     path("logout/", UserLogoutView.as_view(), name="user_logout"),
+
+    path("password-reset/", CostumePasswordResetView.as_view(), name="password_reset")
 ]
