b7dc31c6ef7b03530aed778f921c383d382edee7
Amir Razavi <s.amirhosein.rzv@gmail.com>
2026-02-15 18:12:08 +0330

    just delete the entire templates and urls to implement the whole thing again.

--- 04 - Task Management System/accounts/urls.py
+++ 04 - Task Management System/accounts/urls.py
@@ -6,17 +6,4 @@ urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="user_register"),
     path("login/", UserLoginView.as_view(), name="user_login"),
     path("logout/", UserLogoutView.as_view(), name="user_logout"),
-
-    path("password-reset/", CostumePasswordResetView.as_view(template_name="accounts/password_reset.html"),
-         name="password_reset"),
-
-    path("password-reset/done/", CostumePasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"),
-         name="password_reset_done"),
-
-    path("reset/<uidb64>/<token>/", CostumePasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"),
-         name="password_reset_confirm"),
-
-    path("reset/done/", CostumePasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"),
-         name="password_reset_complete"),
-
 ]
