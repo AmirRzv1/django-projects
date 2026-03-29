87f28fb1f1040a7cf908cb3cd1c774013b7a57c4
Amir Razavi <s.amirhosein.rzv@gmail.com>
2026-03-29 23:41:23 +0330

    create a costume user and add the esential fields.

--- 04 - Task Management System/user_service/accounts/models.py
+++ 04 - Task Management System/user_service/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 class CostumeUserManager(BaseUserManager):
     def create_user(self, username, password, email=None):
@@ -12,12 +12,40 @@ class CostumeUserManager(BaseUserManager):
         if email:
             user.email = self.normalize_email()
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
         return user
 
     def create_superuser(self, username, password, email=None):
         user = self.create_user(username, password, email)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using=self.db)
+        user.save(using=self._db)
         return user
+
+class CostumeUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CostumeUserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
+
+
+
+
+
+
+
+
+
+
+
+
+
