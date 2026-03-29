738e587d5c433eff50e5ab2be39a64f65234485d
Amir Razavi <s.amirhosein.rzv@gmail.com>
2026-03-29 23:58:12 +0330

    create my costume backends to implement the costume authenticate.

--- 04 - Task Management System/user_service/user_service/settings.py
+++ 04 - Task Management System/user_service/user_service/settings.py
@@ -149,3 +149,9 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+AUTHENTICATION_BACKENDS = [
+    'accounts.backends.EmailOrUsernameBackend',
+]
