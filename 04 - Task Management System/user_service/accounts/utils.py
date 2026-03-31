2232d6dd8b2fc4495a3ec83f8c939d5dda5efbd7
Amir Razavi <s.amirhosein.rzv@gmail.com>
2026-03-31 17:38:57 +0330

    update the utils in user_service and now we are good to go

--- 04 - Task Management System/user_service/accounts/utils.py
+++ 04 - Task Management System/user_service/accounts/utils.py
@@ -12,11 +12,13 @@ def generate_jwt_token(user):
         'user_id': user.id,
         'username': user.username,
         'email': user.email,
-        'exp': datetime.utcnow() + timedelta(hours=1),  # Expires in 1 hour
+        'exp': datetime.utcnow() + timedelta(hours=settings.JWT_EXPIRATION_HOURS),
         'iat': datetime.utcnow(),  # Issued at (when token was created)
     }
 
     # settings.SECRET_KEY is used to sign the token
     # All services must have the SAME secret key to verify this token
-    token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+    token = jwt.encode(payload,
+                       settings.JWT_SECRET_KEY,
+                       algorithm=settings.JWT_ALGORITHM)
     return token
