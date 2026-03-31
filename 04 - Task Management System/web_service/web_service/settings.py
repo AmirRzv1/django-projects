f2c663e5693fca2652cd97d0f5f8685e62fb2ddb
Amir Razavi <s.amirhosein.rzv@gmail.com>
2026-03-31 17:37:07 +0330

    create .env file for the web_service and update the setting.

--- 04 - Task Management System/web_service/web_service/settings.py
+++ 04 - Task Management System/web_service/web_service/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g8m7l&*+j3vd%v*prkk6hoyprwlzvguc7b0%r)-wyfjj%%*g(i'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -140,3 +141,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
+
+# Service URLs
+USER_SERVICE_URL = config('USER_SERVICE_URL', default='http://localhost:8001')
+TASK_SERVICE_URL = config('TASK_SERVICE_URL', default='http://localhost:8000')
