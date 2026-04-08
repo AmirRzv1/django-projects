1ae298a52affde2ee638eaa2ccca070668117abd
Amir Razavi <s.amirhosein.rzv@gmail.com>
2026-04-09 00:07:09 +0330

    add basic settings and files for the celery and reddis.

--- 04 - Task Management System/user_service/user_service/settings.py
+++ 04 - Task Management System/user_service/user_service/settings.py
@@ -193,3 +193,11 @@ LOGGING = {
         'level': 'DEBUG',
     },
 }
+
+# Celery Settings
+CELERY_BROKER_URL = config('CELERY_BROKER_URL', default='redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND', default='redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
